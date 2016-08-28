4ce5da10d6ac83121a2caa3847b46a22d57ba490
Russ Rinzler <russ.rinzler@gmail.com>
2016-08-28 13:48:37 -0700

    call requestAnimationFrame on start

--- src/HomageInteriorApp.js
+++ src/HomageInteriorApp.js
@@ -1,9 +1,16 @@
 export default {
     init() {
         this.findDomElements();
+        this.renderAnim();
     },
 
     findDomElements() {
-        this.appEl = window.document.getElementById('homage-to-barragan-interior-app');
+        this.appEl = document.getElementById('homage-to-barragan-interior-app');
+    },
+
+    renderAnim(timestamp) {
+        const time = timestamp || 0;
+        console.log(Math.sin(time / 1000));
+        requestAnimationFrame((time) => this.renderAnim(time));
     }
-}
+};
