6fda598f24d9cdf23034e9bbcebe2c584dd83d99
Russ Rinzler <russ.rinzler@gmail.com>
2016-09-15 23:31:23 -0700

    add a directional light and make the walls taller

--- src/HomageInteriorApp.js
+++ src/HomageInteriorApp.js
@@ -23,61 +23,81 @@ export default {
     },
 
     initializeMeshes() {
-        // center box
+        // center shape
         const boxGeometry =  new THREE.TorusGeometry(0.75, 0.4, 5, 5, 3.4);
         const color = new THREE.Color(0xffffff);
         const material = new THREE.MeshBasicMaterial({ color });
         const boxMesh = new THREE.Mesh(boxGeometry, material);
+        boxMesh.castShadow = true;
         this.scene.add(boxMesh);
 
         // ground plane
-        const planeColor = new THREE.Color(0xdddddd);
+        const planeColor = new THREE.Color(0x90C3D4);
         const planeMaterial = new THREE.MeshLambertMaterial({ color: planeColor });
         const planeGeometry =  new THREE.PlaneGeometry(20, 20);
         const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
         planeMesh.rotation.x = -Math.PI / 2;
         planeMesh.position.y = -0.5;
+        planeMesh.receiveShadow = true;
         this.scene.add(planeMesh);
 
         // back plane
         const backPlaneColor = new THREE.Color(0xeeeeee);
         const backPlaneMaterial = new THREE.MeshLambertMaterial({ color: backPlaneColor });
-        const backPlaneGeometry =  new THREE.PlaneGeometry(20, 8);
+        const backPlaneGeometry =  new THREE.PlaneGeometry(20, 7);
         const backPlaneMesh = new THREE.Mesh(backPlaneGeometry, backPlaneMaterial);
+        backPlaneMesh.position.y = 3;
         backPlaneMesh.position.z = -10;
+        backPlaneMesh.castShadow = true;
+        backPlaneMesh.receiveShadow = true;
         this.scene.add(backPlaneMesh);
 
-        // back plane
+        // front plane
         const frontPlaneColor = new THREE.Color(0xaaaaaa);
         const frontPlaneMaterial = new THREE.MeshLambertMaterial({ color: frontPlaneColor });
-        const frontPlaneGeometry =  new THREE.PlaneGeometry(20, 8);
+        const frontPlaneGeometry =  new THREE.PlaneGeometry(20, 7);
         const frontPlaneMesh = new THREE.Mesh(frontPlaneGeometry, frontPlaneMaterial);
+        frontPlaneMesh.position.y = 3;
         frontPlaneMesh.position.z = 10;
         frontPlaneMesh.rotation.y = Math.PI;
+        frontPlaneMesh.castShadow = true;
+        frontPlaneMesh.receiveShadow = true;
         this.scene.add(frontPlaneMesh);
 
         // left plane
         const leftPlaneColor = new THREE.Color(0xcccccc);
         const leftPlaneMaterial = new THREE.MeshLambertMaterial({ color: leftPlaneColor });
-        const leftPlaneGeometry =  new THREE.PlaneGeometry(20, 8);
+        const leftPlaneGeometry =  new THREE.PlaneGeometry(20, 7);
         const leftPlaneMesh = new THREE.Mesh(leftPlaneGeometry, leftPlaneMaterial);
         leftPlaneMesh.position.x = -10;
+        leftPlaneMesh.position.y = 3;
         leftPlaneMesh.rotation.y = Math.PI / 2;
+        leftPlaneMesh.castShadow = true;
+        leftPlaneMesh.receiveShadow = true;
         this.scene.add(leftPlaneMesh);
 
         // right plane
         const rightPlaneColor = new THREE.Color(0xbbbbbb);
         const rightPlaneMaterial = new THREE.MeshLambertMaterial({ color: rightPlaneColor });
-        const rightPlaneGeometry =  new THREE.PlaneGeometry(20, 8);
+        const rightPlaneGeometry =  new THREE.PlaneGeometry(20, 7);
         const rightPlaneMesh = new THREE.Mesh(rightPlaneGeometry, rightPlaneMaterial);
         rightPlaneMesh.position.x = 10;
+        rightPlaneMesh.position.y = 3;
         rightPlaneMesh.rotation.y = -Math.PI / 2;
+        rightPlaneMesh.castShadow = true;
+        rightPlaneMesh.receiveShadow = true;
         this.scene.add(rightPlaneMesh);
     },
 
     initializeLights() {
         const ambientLight = new THREE.AmbientLight(0x4B4B4B);
         this.scene.add(ambientLight);
+
+
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.75);
+        directionalLight.position.set(0, 1, 0);
+        directionalLight.castShadow = true;
+        this.scene.add(directionalLight);
     },
 
     initializeCamera() {
@@ -98,6 +118,8 @@ export default {
         }
 
         this.appEl.appendChild(this.renderer.domElement);
+        this.renderer.shadowMapEnabled = true;
+        this.renderer.shadowMapType = THREE.PCFSoftShadowMap;
         this.renderer.render(this.scene, this.camera);
     },
 
